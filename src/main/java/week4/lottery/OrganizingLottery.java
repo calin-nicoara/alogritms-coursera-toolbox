2ac83b2446291617a78bd7078c6e32851b703241
calin_nicoara <calin.nicoara@esolutions.ro>
2018-03-19 10:52:14 +0200

    Added naive solution to segment problem.

--- src/main/java/week4/lottery/OrganizingLottery.java
+++ src/main/java/week4/lottery/OrganizingLottery.java
@@ -1,12 +1,40 @@
 package week4.lottery;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class OrganizingLottery {
 
     public static List<Integer> calculateNumberOfSegmentContainingThePoints(
             final List<Integer> points, final List<Segment> segmentList) {
+
+
         return Collections.emptyList();
+    }
+
+    public static List<Integer> calculateNumberOfSegmentContainingThePointsInefficent(
+            final List<Integer> points, final List<Segment> segmentList) {
+        List<Integer> intersectionList = new ArrayList<>();
+        for(int pointIndex = 0; pointIndex < points.size(); pointIndex++) {
+            final int numberIntersections = getNumberIntersections(points, segmentList, pointIndex);
+            intersectionList.add(numberIntersections);
+        }
+
+        return intersectionList;
+    }
+
+    private static int getNumberIntersections(final List<Integer> points, final List<Segment> segmentList, final int pointIndex) {
+        AtomicInteger intersections = new AtomicInteger(0);
+        for(int segmentIndex = 0; segmentIndex < segmentList.size(); segmentIndex++) {
+            final Segment segment = segmentList.get(segmentIndex);
+            if(segment.leftPoint <= points.get(pointIndex)
+                    && segment.rightPoint >= points.get(pointIndex)) {
+                intersections.incrementAndGet();
+            }
+        }
+
+        return intersections.get();
     }
 }
--- src/test/groovy/week4/OrganizingLotterySpec.groovy
+++ src/test/groovy/week4/OrganizingLotterySpec.groovy
@@ -1,13 +1,33 @@
 package week4
 
 import spock.lang.Specification
+import spock.lang.Unroll
 import week4.lottery.OrganizingLottery
 import week4.lottery.Segment
 
 
 class OrganizingLotterySpec extends Specification {
 
-    def "Get numbers of segments that contain the points"() {
+    @Unroll
+    "Get numbers of segments that contain the points"() {
+        when:
+        def result = OrganizingLottery.calculateNumberOfSegmentContainingThePointsInefficent(points, segmentList)
+
+        then:
+        result == numberContainingSegments
+
+        where:
+        points         | segmentList                                                                                                             || numberContainingSegments
+        []             | []                                                                                                                      || []
+        []             | [0, 1]                                                                                                                  || []
+        [1]            | [aSegment()]                                                                                                            || [1]
+        [1, 6, 11]     | [aSegment(leftPoint: 0, rightPoint: 5), aSegment(leftPoint: 7, rightPoint: 10)]                                         || [1, 0, 0]
+        [-100, 100, 0] | [aSegment(leftPoint: -10, rightPoint: 10)]                                                                              || [0, 0, 1]
+        [1, 6]         | [aSegment(leftPoint: 0, rightPoint: 5), aSegment(leftPoint: -3, rightPoint: 2), aSegment(leftPoint: 7, rightPoint: 10)] || [2, 0]
+    }
+
+    @Unroll
+    "Get numbers of segments that contain the points efficient"() {
         when:
         def result = OrganizingLottery.calculateNumberOfSegmentContainingThePoints(points, segmentList)
 
@@ -15,10 +35,13 @@ class OrganizingLotterySpec extends Specification {
         result == numberContainingSegments
 
         where:
-        points | segmentList  || numberContainingSegments
-        []     | []           || []
-        []     | [0, 1]       || []
-        [1]    | [aSegment()] || [1]
+        points         | segmentList                                                                                                             || numberContainingSegments
+        []             | []                                                                                                                      || []
+        []             | [0, 1]                                                                                                                  || []
+        [1]            | [aSegment()]                                                                                                            || [1]
+        [1, 6, 11]     | [aSegment(leftPoint: 0, rightPoint: 5), aSegment(leftPoint: 7, rightPoint: 10)]                                         || [1, 0, 0]
+        [-100, 100, 0] | [aSegment(leftPoint: -10, rightPoint: 10)]                                                                              || [0, 0, 1]
+        [1, 6]         | [aSegment(leftPoint: 0, rightPoint: 5), aSegment(leftPoint: -3, rightPoint: 2), aSegment(leftPoint: 7, rightPoint: 10)] || [2, 0]
     }
 
     static aSegment(Map overrides = [:]) {
